7c73f80ef824bdaa46d43d33c63f9724ce85b9f9
Drastic_Coder <deepbansodeofficial4205@gmail.com>
2022-09-23 16:27:23 +0530

    Add teacher routes

---
+++ routes/teacher.js
@@ -0,0 +1,59 @@
+
+// import express
+const express = require('express');
+const router = express.Router();
+
+// import teacher model
+const Teacher = require('../models/Teacher');
+const Student = require('../models/Student');
+
+// test route
+router.get('/', (req, res) => {
+    res.send("This is teacher route");
+});
+
+
+// get all students
+// router.get('/', (req, res) => {
+//     Student.find({}, (err, students) => {
+//         if (err) {
+    //             console.log(err);
+    //         } else {
+        //             res.json(students);
+        //         }
+        //     });
+        // }
+        // );
+        
+// get students of teacher
+router.get('/allocated-students/:username', (req, res) => {
+    Teacher.findOne({ username: req.params.username }, (err, teacher) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (teacher) {
+                res.json(teacher.students);
+
+
+            } else {
+                res.json({ message: 'Invalid username' });
+            }
+        }
+    });
+});
+        
+// get student by id
+router.get('/:id', (req, res) => {
+    Student.findById(req.params.id, (err, student) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(student);
+        }
+    });
+}
+);
+
+
+// export router
+module.exports = router;
