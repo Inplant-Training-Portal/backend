2dc36dbb3f94850dfa961c9233177d4bfb551e3a
Drastic_Coder <deepbansodeofficial4205@gmail.com>
2022-09-23 16:25:43 +0530

    Create teacher model

---
+++ models/Teacher.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// teacher schema
+const TeacherSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    students: {
+        type: Array
+    }
+});
+
+// export teacher model
+module.exports = mongoose.model('Teacher', TeacherSchema);
