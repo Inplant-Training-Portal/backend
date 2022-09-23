e0c7ce52b3134ad6c58d5d48b388c46a1b61ac82
Drastic_Coder <deepbansodeofficial4205@gmail.com>
2022-09-23 16:29:12 +0530

    fix : issue models not found

---
+++ index.js
@@ -0,0 +1,45 @@
+// dotenv config
+require('dotenv').config();
+
+// import required modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = 9000;
+
+// use cors middleware
+app.use(cors());
+// use body-parser middleware
+app.use(bodyParser.json());
+
+// connect to mongodb
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log('MongoDB database connection established successfully!');
+    }
+);
+
+// import routes
+const studentRoutes = require('./routes/student');
+const teacherRoutes = require('./routes/teacher');
+const adminRoutes = require('./routes/admin');
+
+// use routes
+app.use('/student', studentRoutes);
+app.use('/teacher', teacherRoutes);
+app.use('/admin', adminRoutes);
+
+// test route
+app.get('/', (req, res) => {
+    res.send("testing");
+});
+
+// start server
+app.listen(port, function() {
+    console.log('Server is running on port: ' + port);
+});
