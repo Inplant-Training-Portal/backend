fe9cda7cd942d2361739cdff36aeaeb3a7db24b2
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-12-22 14:18:30 +0530

    refactor: remove unwanted routes & use passport middleware to authenticate routes

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
+const passport = require('passport')
+
+// import auth file
+require('../auth/student.auth')(passport)
 
 // import student controller
 const studentController = require('../controllers/student.controller');
@@ -11,19 +15,19 @@ const studentController = require('../controllers/student.controller');
 router.post('/login', studentController.loginStudent);
 
 // update student profile
-router.post('/update/info/:id', studentController.updateStudentInfo);
+router.post('/update/info', passport.authenticate('jwt', { session: false }), studentController.updateStudentInfo);
 
 // change password
-router.post('/update/password/:id', studentController.changeStudentPassword);
+router.post('/update/password', passport.authenticate('jwt', { session: false }), studentController.changeStudentPassword);
 
 // get teacher profile
-router.get('/teacher/:id', studentController.getTeacherProfile);
+// router.get('/teacher/:id', studentController.getTeacherProfile);
 
 // upload document
-router.post("/file/upload/:id", upload.single('file'), studentController.uploadFile);
+router.post("/file/upload", passport.authenticate('jwt', { session: false }), upload.single('file'), studentController.uploadFile);
 
 // map document
-router.get("/file/map/:id", studentController.mapFile);
+router.get("/file/map", passport.authenticate('jwt', { session: false }), studentController.mapFile);
 
 
 // export router
