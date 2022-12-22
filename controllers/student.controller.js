a893847f640ef489c34687269a4427d1726516a2
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-12-22 14:16:47 +0530

    fix: use async/await, try/catch & optimized queries

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -1,258 +1,255 @@
+// import packages
+const bcrypt = require('bcrypt');
 const { google } = require('googleapis');
 const keys = require('../inplant-training-portal-369403-dec6828816c4.json');
 const fs = require('fs');
+const jwt = require('jsonwebtoken')
 
-// import the student model
+
+// import models
 const Student = require('../models/Student.model');
 const Teacher = require('../models/Teacher.model');
 const File = require('../models/File.model');
 
-// import packages
-const bcrypt = require('bcrypt');
+// saltRounds for encrypting password
+const saltRounds = 12
 
-// login
-const loginStudent = (req, res) => {
-    const { enrollment_no, password } = req.body;
-    Student.findOne({ enrollment_no }, function (err, student) {
-        if (err) {
-            res.status(500).json({
-                error: err
-            });
+
+// login student
+const loginStudent = async (req, res) => {
+    
+    try {
+        const { enrollment_no, password } = req.body;
+        let student = await Student.findOne({ enrollment_no: enrollment_no })
+        if(!student) {
+            res.status(404).json({
+                message: "Oops, No Student Found!"
+            })
         }
-        if (student) {
+        else {
             // compare password
-            bcrypt.compare(password, student.password, function (err, result) {
-                if (err) {
-                    res.status(401).json({
-                        message: 'Login failed! Please try again.'
-                    });
+            bcrypt.compare(password, student.password, function(err, result) {
+                if(result) {
+                    const payload = { id: student._id, enrollment_no: student.enrollment_no, name: student.name, email: student.email, mobile_no: enrollment_no.mobile_no }
+
+                    // sign jwt to get token
+                    jwt.sign(
+                        payload,
+                        process.env.SECRETORKEY,
+                        { expiresIn: '1h' },
+                        (err, token) => {
+                            res.status(202).json({
+                                message: "Login Successful!",
+                                token: "Bearer " + token,
+                                user: payload
+                            })
+                        }
+                    )
                 }
-                if (result) {
-                    res.status(200).json({
-                        message: 'Login successful!',
-                        user: student
-                    });
-                } else {
+                else {
                     res.status(401).json({
-                        message: 'Password does not match!'
-                    });
+                        message: "Incorrect Credentials"
+                    })
                 }
-            });
-        } else {
-            res.status(401).json({
-                message: 'Oops, Student not found!'
-            });
+            })
         }
-    });
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({
+            error: err,
+            message: "Something went wrong"
+        })
+    }
 }
 
 // update student info
-const updateStudentInfo = (req, res) => {
-    const id = req.params.id;
+const updateStudentInfo = async (req, res) => {
+    
+    try {
+        const id = req.user.id;
+        let newInfo = {};
 
-    let newInfo = {};
+        if(req.body.email) {
+            newInfo.email = req.body.email;
+        }
 
-    if(req.body.email){
-        newInfo.email = req.body.email;
+        if(req.body.mobile_no) {
+            newInfo.mobile_no = req.body.mobile_no
+        }
 
-    }
+        await Student.findByIdAndUpdate(id, newInfo, { new: true })
 
-    if(req.body.mobile_no) {
-        newInfo.mobile_no = req.body.mobile_no;
-    }
-
-    Student.findByIdAndUpdate(id, newInfo, { new: true })
-        .then(function (result) {
-            res.status(200).json({
-                message: 'Info updated successfully!',
-                result
-            });
+        res.status(200).json({
+            message: "Info update successfully!"
         })
-        .catch(function (err) {
-            res.status(500).json({
-                error: err
-            });
-        });
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({
+            error: err,
+            message: "Something went wrong"
+        })
+    }
 }
 
 // change password
-const changeStudentPassword = (req, res) => {
-    const id = req.params.id;
-    const { oldPassword, newPassword, confirmPassword } = req.body;
+const changeStudentPassword = async (req, res) => {
+    
+    try {
+        const id = req.user.id;
+        const { oldPassword, newPassword, confirmPassword } = req.body;
+        
+        let student = await Student.findById(id)
 
-    // check if old password is correct
-    if(oldPassword) {
-        Student.findById(id)
-            .then(function (student) {
-                if (student) {
-                    // compare password
-                    bcrypt.compare(oldPassword, student.password, function (err, result) {
-                        if (err) {
-                            res.status(401).json({
-                                message: 'Login failed! Please try again.'
-                            });
+        // compare password
+        bcrypt.compare(oldPassword, student.password, function (err, result) {
+            if(result) {
+                if(newPassword === confirmPassword) {
+                    // encrypt password
+                    bcrypt.hash(newPassword, saltRounds, function(err, hashedPassword) {
+                        if(result) {
+                            student.password = hashedPassword
+                            student.save()
+
+                            res.status(202).json({
+                                message: "Password changed successfully"
+                            })
                         }
-                        if (result) {
-                            // check if new password and confirm password match
-                            if(newPassword === confirmPassword) {
-                                // encrypt password
-                                bcrypt.hash(newPassword, 10, function (err, hash) {
-                                    if (err) {
-                                        res.json({
-                                            error: err
-                                        })
-                                    }
-                                    Student.findByIdAndUpdate(id, { password: hash }, { new: true })
-                                        .then(function (result) {
-                                            res.status(200).json({
-                                                message: 'Password updated successfully!',
-                                                result
-                                            });
-                                        })
-                                        .catch(function (err) {
-                                            res.status(500).json({
-                                                error: err
-                                            });
-                                        });
-                                });
-                            } else {
-                                res.status(401).json({
-                                    message: 'New password and Confirm password do not match!'
-                                });
-                            }
-                        }
-                        else {
-                            res.status(401).json({
-                                message: 'Old password does not match!'
-                            });
-                        }
-                    });
-                } else {
-                    res.status(401).json({
-                        message: 'Oops, Student not found!'
-                    });
+                    })
+                }
+                else {
+                    res.status(400).json({
+                        message: "New password and Confirm password do not match!"
+                    })
                 }
             }
-            )
-            .catch(function (err) {
-                res.status(500).json({
-                    error: err
-                });
-            }
-            );
-    }
-}
-
-// get student profile
-const getTeacherProfile = async (req, res) => {
-    try {
-        // request student is _id of student
-        const teacher = await Teacher.findById(req.teacher.id);
-        res.json(teacher);
-    } catch (e) {
-        res.send({ message: 'Error in Fetching teacher' });
-    }
-}
-
-// upload file
-const uploadFile = (req, res) => {
-    const student_id = req.params.id;
-    const { originalname, mimeType, path } = req.file;
-
-    const client = new google.auth.JWT(
-        keys.client_email,
-        null,
-        keys.private_key,
-        ['https://www.googleapis.com/auth/drive']
-    );
-
-    client.authorize(function(err, tokens) {
-        if(err) {
-            console.log(err);
-            return;
-        }
-        else {
-            console.log('Connected to Drive API');
-            gdrun(client);
-        }
-    });
-
-    async function gdrun(cl) {
-        const gdapi = google.drive({ version: 'v3', auth: cl });
-
-        const fileMetadata = {
-            'name': originalname,
-            'parents': ['1F3GpRYV4cme9EGaXwV50grTnWfEhyM5M']
-        };
-
-        const media = {
-            mimeType: mimeType,
-            body: fs.createReadStream(path)
-        };
-
-        const file = await gdapi.files.create({
-            resource: fileMetadata,
-            media: media,
-            fields: 'id'
-        });
-
-        
-        Student.findById(student_id)
-        .then(function (student) {
-            if (student) {
-                const fileData = new File({
-                    student_id: student._id,
-                    name: originalname,
-                    url : `https://drive.google.com/file/d/${file.data.id}`
-                });
-                fileData.save()
-                
-                student.documents.push(fileData._id);
-                student.save();
-                
+            else {
+                res.status(401).json({
+                    message: "Old password doesn't match"
+                })
             }
         })
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Something went wrong"
+        })
+    }
+}
+
+// // get student profile
+// const getTeacherProfile = async (req, res) => {
+//     try {
+//         // request student is _id of student
+//         const teacher = await Teacher.findById(req.teacher.id);
+//         res.json(teacher);
+//     } catch (e) {
+//         res.send({ message: 'Error in Fetching teacher' });
+//     }
+// }
+
+// upload file
+const uploadFile = async (req, res) => {
+
+    try {
+        const id = req.user.id;
+        const { originalname, mimeType, path } = req.file;
+
+        const client = new google.auth.JWT(
+            keys.client_email,
+            null,
+            keys.private_key,
+            ['https://www.googleapis.com/auth/drive']
+        );
         
-        fs.unlink(path, (err) => {
+        client.authorize(async function(err, tokens) {
             if(err) {
                 console.log(err);
+                return;
             }
             else {
-                console.log('File deleted');
+                console.log('Connected to Drive API');
+                await gdrun(client);
             }
         });
+        
+        async function gdrun(cl) {
+            const gdapi = google.drive({ version: 'v3', auth: cl });
+    
+            const fileMetadata = {
+                'name': originalname,
+                'parents': ['1F3GpRYV4cme9EGaXwV50grTnWfEhyM5M']
+            };
+    
+            const media = {
+                mimeType: mimeType,
+                body: fs.createReadStream(path)
+            };
+    
+            const file = await gdapi.files.create({
+                resource: fileMetadata,
+                media: media,
+                fields: 'id'
+            });
 
-        res.status(200).json({
-            message: 'File uploaded successfully!'
-        });
-    }
+            console.log("File uploaded to Google Drive");
 
-    // call the function
-    gdrun().then(() => {
-        console.log('File uploaded successfully.');
+            let student = await Student.findById(id)
+
+            const fileData = await File.create({
+                student_id: student._id,
+                name: originalname,
+                url : `https://drive.google.com/file/d/${file.data.id}`
+            });
+
+            student.documents.push(fileData._id)
+            student.save()
+            
+            fs.unlink(path, (err) => {
+                if(err) {
+                    console.log(err);
+                }
+                else {
+                    console.log('Local file deleted');
+                }
+            });
+    
+            res.status(201).json({
+                message: 'File uploaded successfully!'
+            });
+        }
     }
-    ).catch((err) => {
+    catch(err) {
         console.log(err);
-    });
+        res.status(500).json({
+            message: "Something went wrong"
+        })
+    }
+
+
+
 }
 
 // map file
-const mapFile = (req, res) => {
-    const id = req.params.id
+const mapFile = async (req, res) => {
 
-    // get students id from params and return all documents
-    File.find({ student_id: id })
-        .then(function (documents) {
-            res.status(200).json({
-                documents
-            });
-        }
-        )
-        .catch(function (err) {
-            res.status(500).json({
-                error: err
-            });
-        });
+    try {
+        const id = req.user.id
+
+        let documents = await File.find({ student_id: id })
+
+        res.status(200).json({
+            documents
+        })
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Something went wrong"
+        })
+    }
 }
 
 
@@ -260,7 +257,7 @@ module.exports={
     loginStudent,
     updateStudentInfo,
     changeStudentPassword,
-    getTeacherProfile,
+    // getTeacherProfile,
     uploadFile,
     mapFile
 }
